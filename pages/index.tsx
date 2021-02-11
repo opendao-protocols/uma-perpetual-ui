430574273d2602477938408f5af07ddfde1a6d19
akshaysrivastav <akshaysrivastav2014@gmail.com>
2021-02-11 12:33:57 +0530

    Main tab selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,14 @@ import {
   Tabs,
   Hidden,
   Button,
-  IconButton,
   Menu,
   MenuItem,
   Grid,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
+import PerpAddresses from "../containers/PerpAddresses";
+import Collateral from "../containers/Collateral";
 import Header from "../features/core/Header";
 import PerpSelector from "../features/perp-selector/PerpSelector";
 import Create from "../features/manage-position/Create";
@@ -31,12 +32,94 @@ const StyledTabs = styled(Tabs)`
 `;
 
 export default function Index() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedMenuItem, setSelectedMenuItem] = useState<string>(
+    "Mint y-Dollars"
+  );
+  const [options, setOptions] = useState<Array<string>>([]);
+  const { perpAddress } = PerpAddresses.useContainer();
+  const { address: collAddress } = Collateral.useContainer();
+
+  const buildOptionsList = () => {
+    // Default list that all contracts have.
+    let menuOptions = ["Mint y-Dollars", "My Positions"];
+
+    // Update selected page if the user toggles between Perps while selected on
+    // invalid pages (i.e on Wrap/Unwrap then moves to uUSDrBTC).
+    if (menuOptions.indexOf(selectedMenuItem) == -1) {
+      setSelectedMenuItem("Mint y-Dollars");
+    }
+    return menuOptions;
+  };
+
+  useEffect(() => {
+    setOptions(buildOptionsList());
+  }, [perpAddress, collAddress, selectedMenuItem]);
+
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (index: number) => {
+    setAnchorEl(null);
+    setSelectedMenuItem(options[index]);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Container maxWidth={"md"}>
       <Box py={4}>
         <Header />
         <PerpSelector />
-        <Create />
+        <Hidden only={["sm", "xs"]}>
+          <StyledTabs
+            value={options.indexOf(selectedMenuItem)}
+            onChange={(_, index) => handleMenuItemClick(index)}
+          >
+            {options.map((option, index) => (
+              <Tab key={index} label={option} disableRipple />
+            ))}
+          </StyledTabs>
+        </Hidden>
+        <Hidden only={["md", "lg", "xl"]}>
+          <div>
+            <Box pt={1} pb={2}>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <Button variant="outlined" onClick={handleClickListItem}>
+                    <MenuIcon />
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Typography style={{ marginTop: `8px` }}>
+                    <strong>Current page:</strong> {selectedMenuItem}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {options.map((option, index) => (
+                <MenuItem
+                  key={index}
+                  selected={index === options.indexOf(selectedMenuItem)}
+                  onClick={(_) => handleMenuItemClick(index)}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </Hidden>
+        {selectedMenuItem === "Mint y-Dollars" && <Create />}
+        {selectedMenuItem === "My Positions" && <i>TODO</i>}
       </Box>
     </Container>
   );
