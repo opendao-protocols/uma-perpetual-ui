5d32a3e55756cf6fa2dc26c09d6ea11ff3c88be4
akshaysrivastav <akshaysrivastav2014@gmail.com>
2021-02-12 14:49:44 +0530

    UI fix

--- features/manage-position/ManagePosition.tsx
+++ features/manage-position/ManagePosition.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Box, Typography } from "@material-ui/core";
 
 import Connection from "../../containers/Connection";
+import PerpAddresses from "../../containers/PerpAddresses";
 import MethodSelector from "./MethodSelector";
 import Deposit from "./Deposit";
 import Redeem from "./Redeem";
@@ -10,27 +11,27 @@ import YourPosition from "./YourPosition";
 import YourLiquidations from "./YourLiquidations";
 import YourWallet from "./YourWallet";
 
-export type Method = "redeem" | "deposit" | "withdraw" | "settle";
+export type Method = "redeem" | "deposit" | "withdraw";
 const DEFAULT_METHOD = "redeem";
 
 const Manager = () => {
   const { signer } = Connection.useContainer();
+  const { perpAddress } = PerpAddresses.useContainer();
   const [method, setMethod] = useState<Method>(DEFAULT_METHOD);
   const handleChange = (e: React.ChangeEvent<{ value: unknown }>) =>
     setMethod(e.target.value as Method);
 
-  if (signer !== null) {
-    useEffect(() => {
-      setMethod(DEFAULT_METHOD);
-    }, []);
+  useEffect(() => {
+    setMethod(DEFAULT_METHOD);
+  }, []);
 
+  if (signer !== null && perpAddress !== null) {
     return (
       <Box my={0}>
         <YourWallet />
         <YourLiquidations />
         <YourPosition />
         <MethodSelector method={method} handleChange={handleChange} />
-
         {method === "redeem" && <Redeem />}
         {method === "deposit" && <Deposit />}
         {method === "withdraw" && <Withdraw />}
